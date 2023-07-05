c1618ec46cbb997fadad60ece0f7e00c776f21be
Dana778 <107307314+Dana778@users.noreply.github.com>
2023-07-05 12:37:14 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-import {Header} from './components/Header';
-import {Body} from './components/Body';
-import {Footer} from './components/Footer';
+import { Header } from './components/Header';
+import { Body } from './components/Body';
+import { Footer } from './components/Footer';
 
-import{
+import {
   Routes,
-  Route, 
+  Route,
   BrowserRouter,
 } from 'react-router-dom'
 
@@ -15,23 +15,68 @@ const App = () => {
   const hHeade = 'Загловок';
   const hBodi = 'Тело';
   const hFoote = 'Нижний колонтитул';
- 
+
+  const [pocemons, setPocemons] = useState([])
+  const [statePoc, setStatePoc] = useState(false)
+
+  const getResponse = () => {
+    fetch('http://localhost:3001/pokemon')
+        .then(
+          (response) => {
+            return (
+              response.json()
+
+            )
+          }
+        )
+        .then(
+          (data) => {
+            setPocemons(data)
+          }
+        )
+        .catch(
+          () => {
+            console.log('проблема с доступом к серверу')
+          }
+        )
+  }
+
   return (
     <>
-    <BrowserRouter>
-    <Header hHeader={hHeade}/>
-    <Routes>
-      <Route path='/' element={
-        <>
-        <Body hBody={hBodi}/>
-        <Footer hFooter={hFoote}/> 
-        </>
-      }/>
-      <Route path='/dashboard' element={
-        <Footer hFooter={hFoote}/>  
-      }/>
-    </Routes>
-    </BrowserRouter>
+      {
+        pocemons ? (
+          pocemons.map((item) => {
+            return (
+              <li key={`${item.id}_marat`}> {item.name}</li>
+            )
+          }
+          )
+        ) : <p>error</p>
+      }
+      <button onClick={
+        () => {
+          setStatePoc(!statePoc)
+          if (statePoc) {
+            getResponse()
+          } else {
+            setPocemons([])
+          }
+        }
+      }>AAAA</button>
+      <BrowserRouter>
+        <Header hHeader={hHeade} />
+        <Routes>
+          <Route path='/' element={
+            <>
+              <Body hBody={hBodi} />
+              <Footer hFooter={hFoote} />
+            </>
+          } />
+          <Route path='/dashboard' element={
+            <Footer hFooter={hFoote} />
+          } />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
